60a37089e8a217f624e00a233654e48ec4e0bfb4
yogeshpalkar-collab <yogesh.palkar@gmail.com>
2025-08-30 18:07:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from SmartApi.smartConnect import SmartConnect
+from SmartApi import SmartConnect
 import pyotp
 import os
 import datetime
